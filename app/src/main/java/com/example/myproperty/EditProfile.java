30ccb28dc247a132a329f344c6d55033602d4cca
dinaerawatya <dinatyaerawaty@gmail.com>
2020-01-05 10:55:41 +0700

    EditProfile

--- app/src/main/java/com/example/myproperty/EditProfile.java
+++ app/src/main/java/com/example/myproperty/EditProfile.java
@@ -2,6 +2,9 @@ package com.example.myproperty;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class EditProfile extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class EditProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile2);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void updateprofile(Profile profile) {
+        /**
+         * Baris kode yang digunakan untuk mengupdate data profile
+         * yang sudah dimasukkan di Firebase Realtime Database
+         */
+        database.child("profile") //akses parent index, ibaratnya seperti nama tabel
+                .child(profil.getKey()) //select data berdasarkan key
+                .setValue(profil) //set value data yang baru
+                .addOnSuccessListener(this, new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+                        /**
+                         * Baris kode yang akan dipanggil apabila proses update barang sukses
+                         */
+                        Snackbar.make(findViewById(R.id.bt_submit), "Data berhasil diupdatekan", Snackbar.LENGTH_LONG).setAction("Oke", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                finish();
+                            }
+                        }).show();
+                    }
+                });
     }
 }
